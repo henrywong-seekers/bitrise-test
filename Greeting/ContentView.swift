0cc21e4750e657ae8906a5a0aa83c3fb95b210eb
whs <hwong@seekerscapital.com>
2020-02-17 16:40:20 +0800

    Initial Commit

---
+++ Greeting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Greeting
+//
+//  Created by Henry Wong on 17/2/2020.
+//  Copyright © 2020 Seekers Capital. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
